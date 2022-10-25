9eb286ce2cb5309bd4b3d7a729be4f564f83d4e5
Nasser <nmaif7788@hotmail.com>
2022-10-25 13:51:19 +0300

    add alert password

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let form = document.getElementById("form");
 let euer = document.querySelector("#user");
+let password = document.getElementById("password");
 
 form.addEventListener("submit", (elm) => {
   elm.preventDefault();
-  alert(`Hello ${euer.value}`);
+  alert(`Hello ${euer.value}, your password is ${password.value}`);
 });
